77401991bf0945d77123cd72ed0df9afa106da23
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-10-09 01:00:31 +0100

    Create all.ts

---
+++ src/items/all.ts
@@ -0,0 +1,17 @@
+export interface Shoe {
+  price: number;
+  name: string;
+  logo: string;
+  image: string;
+}
+
+const Shoes: Array<Shoe> = [
+  {
+    price: 200,
+    name: "Badidas",
+    logo: "Badidas.png",
+    image: "Shoe1.jpg",
+  },
+];
+
+export default Shoes;
